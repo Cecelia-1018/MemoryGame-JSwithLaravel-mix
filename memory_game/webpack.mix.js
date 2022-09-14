5b9d629e537cae1167eaf9173216acf9b0b605f2
Cecelia-1018 <jieshi1018@gmail.com>
2022-09-14 09:33:42 +0800

    add up browserSync

--- memory_game/webpack.mix.js
+++ memory_game/webpack.mix.js
@@ -3,3 +3,6 @@ let mix = require('laravel-mix');
 
 // bundle JavaScript assets + compile Sass files
 mix.js('src/app.js', 'dist/app.js').sass('src/css/layout/app.scss','dist/app.css');
+
+// browser Sync
+mix.browserSync('http://127.0.0.1:5500/memory_game/');
